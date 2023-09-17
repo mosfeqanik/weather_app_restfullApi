3222d0931c5ea71124e415c9e25a9ab37c7132e0
mosfeqanik <mosfeqanik01@gmail.com>
2023-09-17 17:41:51 +0600

    search with city name added

---
+++ lib/utilies/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 40.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 30.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+  color: Colors.black87,
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 40.0,
+);
